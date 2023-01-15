5cfe4b0c8405023ad366e8b4eb3684873cf5a909
simplly <takuyax0806@docomo.ne.jp>
2023-01-15 15:34:54 +0900

    commit

--- memo.rb
+++ memo.rb
@@ -45,11 +45,52 @@ end
 
 ## ユークリッド互除
 
+### 探索
 
+## 幅優先
 
+require 'set'
 
+n = gets.chomp.to_i
 
+done = Set[1]
 
+g = {}
+
+n.times {
+	a, b = gets.split.map(&:to_i)
+	if g.key?(a)
+		g[a] << b
+	else
+		g[a] = [b]
+	end
+	if g.key?(b)
+		g[b] << a
+	else
+		g[b] = [a]
+	end
+}
+
+queue = []
+queue.push(1)
+done << 1
+
+ans = 1
+while !queue.empty?
+	now = queue.shift
+	if g.key?(now)
+		to = g[now]
+		to.each {|nv|
+			if !done.include?(nv)
+				queue.push(nv)
+				done << nv
+				ans = [ans, nv].max
+			end
+		}
+	end
+end
+
+puts ans
 
 
 
